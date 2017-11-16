db3a612eb37fcd7a50ea69593750f718fde9de59
dorota.janicka <dorota.janicka@cgi.com>
2017-11-16 13:36:21 +0100

    zmiana położenia plików oraz utworzenie dobrego mocka

--- src/test/java/springinaction/knights/BraveKnightTest.java
+++ src/test/java/springinaction/knights/BraveKnightTest.java
@@ -9,7 +9,13 @@ import org.junit.Test;
 
 public class BraveKnightTest {
 
+    @Test
     public void knightShouldEmbarkOnQuest(){
-        Quest mockQuest =mock(Quest.class);
+        Quest mockQuest = mock(Quest.class);
+
+        BraveKnight knight = new BraveKnight(mockQuest);
+        knight.embarkOnQuest();
+
+        verify(mockQuest, times(1)).embark();
     }
 }
